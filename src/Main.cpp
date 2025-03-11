1c820a0f822e63b94531ca469a465c02b106aecc
ArcVaw <94964623+archievawser@users.noreply.github.com>
2025-03-11 21:34:39 +1030

    Add basic template

---
+++ src/Main.cpp
@@ -0,0 +1,27 @@
+﻿#include "raylib/raylib.h"
+#include "raylib/raymath.h"
+
+
+int main(void)
+{
+	const int screenWidth = 1920;
+	const int screenHeight = 1080;
+
+	SetConfigFlags(FLAG_MSAA_4X_HINT);
+	InitWindow(screenWidth, screenHeight, "Hello, world!");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
